6f93108cd7882a4e001259c180a521e51b06e64d
Tanjim Hossain <tangimhossain1@gmail.com>
2023-01-13 20:34:53 +0600

    deno

---
+++ wasm-latency/index.js
@@ -0,0 +1,33 @@
+function benchmark(instance, times, iterations) {
+  let totalTime = 0;
+  let result = 0;
+
+  for (let i = 0; i < iterations; i++) {
+    const start = performance.now();
+
+    for (let i = 0; i < times; i++) {
+      result = instance.exports.foo(result);
+    }
+
+    const end = performance.now();
+    totalTime += end - start;
+  }
+
+  return {
+    totalTime: totalTime / iterations,
+    result,
+  };
+}
+
+(async () => {
+  const response = await fetch("http://127.0.0.1:5500/wasm-latency/noop.wasm", { cache: "force-cache" }).then(
+    (response) => response.arrayBuffer()
+  );
+  const module = new WebAssembly.Module(response);
+  const instance = new WebAssembly.Instance(module);
+
+  benchmark(instance, 10_000_000, 10);
+  const { totalTime, result } = benchmark(instance, 1_000_000, 10);
+
+  alert("WASM Latency: " + totalTime + "ms" + "\n" + "WASM Result: " + result);
+})();
--- wasm-latency/noop.wat
+++ wasm-latency/noop.wat
@@ -1,1 +1,8 @@
-(module (func (export "foo") (result i32) i32.const 0 return))
+(module 
+    (func (export "foo") (param i32) (result i32) 
+      local.get 0
+      i32.const 1
+      i32.add
+      return 
+    )
+)
